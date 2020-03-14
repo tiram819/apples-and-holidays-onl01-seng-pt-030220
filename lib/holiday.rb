f29968fa3d8e76ea1a1cc48622bb7b34c1eebf04
Tabassum Hassan <tiram819@gmail.com>
2020-03-14 20:20:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,9 @@ end
 
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
+  array = holiday_hash[:winter].map do |holiday, items|
+    items
+  end
 
 end
 
